322e0b13b697d4b7691b1b2097d0057e69f0819e
Ayush Rathore <aayushh79@gmail.com>
2021-04-24 22:37:46 +0530

    Add greeting-app.py

--- greeting-app.py
+++ greeting-app.py
@@ -19,7 +19,6 @@ Screen:
     MDTextField:
         id: text
         hint_text: 'Enter your name'
-        helper_text: 'Please keep the first letter of your name Capital'
         helper_text_mode: "on_focus"
         pos_hint: {'center_x': 0.5, 'center_y': 0.6}
         size_hint_x: None
