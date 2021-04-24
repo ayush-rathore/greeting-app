2f893c6abcf6a02ef313e87967694d69cbf88a51
Ayush Rathore <aayushh79@gmail.com>
2021-04-24 21:50:50 +0530

    Added Files

---
+++ greeting-app.py
@@ -0,0 +1,58 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+
+kv = """
+Screen:
+    in_class: text
+    MDLabel:
+        text: 'Greeting App'
+        font_style: "H3"
+        pos_hint: {'center_x': 0.83, 'center_y': 0.8}
+
+    MDLabel:
+        text: "(Don't judge me on my appearance, I'm trying to get better everyday.)"
+        pos_hint: {'center_x': 0.7, 'center_y': 0.725}
+
+    MDTextField:
+        id: text
+        hint_text: 'Enter your name'
+        helper_text: 'Please keep the first letter of your name Capital'
+        helper_text_mode: "on_focus"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.6}
+        size_hint_x: None
+        width: 300
+        required: True
+        icon_right : "human-greeting"
+
+    MDRectangleFlatButton:
+        text: 'Submit'
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        on_press:
+            app.greet()
+
+    MDLabel:
+        text: ''
+        id: show
+        pos_hint: {'center_x': 0.95, 'center_y': 0.2}
+
+
+"""
+class MainApp(MDApp):
+    in_class = ObjectProperty(None)
+
+    def build(self):
+        return Builder.load_string(kv)
+
+    def greet(self):
+        self.dialog = MDDialog(title='Greeting you', text="Hello %s!" %(self.root.in_class.text), size_hint=(0.8,1), buttons=[MDFlatButton(text='Thank You', on_release=self.close_dialog)])
+        self.dialog.open()
+
+    def close_dialog(self,obj):
+        self.dialog.dismiss()
+
+MainApp().run()
+
+
